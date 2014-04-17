01bb0d1f565ed086b174532779da089e47efd5dc
Kian Ryan <kian@orangetentacle.co.uk>
2014-04-17 07:37:25 +0100

    Chapter 8 Playtime
    * Date and Time to Sidebar
    * Set currency locale for en
    * Added functional test to product maintenance

--- depot/test/controllers/products_controller_test.rb
+++ depot/test/controllers/products_controller_test.rb
@@ -15,6 +15,8 @@ class ProductsControllerTest < ActionController::TestCase
     get :index
     assert_response :success
     assert_not_nil assigns(:products)
+
+    assert_select ".list_description dl", 3
   end
 
   test "should get new" do
