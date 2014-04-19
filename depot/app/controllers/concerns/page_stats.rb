8ed650a33b508467a8389244f96bc3d2ddca10be
Kian Ryan <kian@orangetentacle.co.uk>
2014-04-19 14:23:50 +0100

    Switched assignent of session to find and rescue

--- depot/app/controllers/concerns/page_stats.rb
+++ depot/app/controllers/concerns/page_stats.rb
@@ -4,8 +4,10 @@ module PageStats
   private
 
     def visit
-      session[:page_visit] |= 0
       session[:page_visit] += 1
+    rescue
+      session[:page_visit] = 1
+    ensure
       @visit = session[:page_visit]
     end
 
